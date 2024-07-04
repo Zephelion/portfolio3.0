d814833afc2a63c7f444defbdda54ac564087205
Zephelion <russellnumo@outlook.com>
2024-07-04 17:32:11 +0200

    feature: added some easing for transition

--- src/components/features/Loader/Loader.tsx
+++ src/components/features/Loader/Loader.tsx
@@ -1,24 +1,21 @@
 import { Section } from "@/components/features";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Text } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import { MotionBox } from "../MotionBox";
 
 export const Loader = () => {
   const [progress, setProgress] = useState(0);
   const [shouldAnimate, setShouldAnimate] = useState(false);
 
   useEffect(() => {
-    // Set up an interval to increment the progress
     const interval = setInterval(() => {
       setProgress((prevProgress) => {
         if (prevProgress >= 100) {
-          clearInterval(interval); // Stop the interval when progress reaches 100
+          clearInterval(interval);
 
-          setTimeout(() => setShouldAnimate(true), 2000); // Wait for 500ms before animating out
+          setTimeout(() => setShouldAnimate(true), 2000); // Wait for 2s before animating out
           return 100;
         }
-        return prevProgress + 1; // Increment progress
+        return prevProgress + 1;
       });
     }, 10);
 
@@ -26,27 +23,23 @@ export const Loader = () => {
   }, []);
 
   return (
-    <motion.div
-      initial={{ y: 0 }}
-      animate={{ y: shouldAnimate ? "-100vh" : 0 }}
-      transition={{ duration: 0.5 }}
+    <Section
+      height="100vh"
+      width="100vw"
+      bg="black"
+      position="absolute"
+      top="0"
+      display="flex"
+      flexDirection="column"
+      justifyContent="flex-end"
+      transform={shouldAnimate ? "translateY(-100vh)" : "translateY(0)"}
+      transition="transform 1s cubic-bezier(0.4, 0.0, 0.2, 1)"
+      zIndex={10}
     >
-      <Section
-        height="100vh"
-        width="100vw"
-        bg="black"
-        position="fixed"
-        top="0"
-        display="flex"
-        flexDirection="column"
-        justifyContent="flex-end"
-      >
-        <Heading as="span" color="white" fontSize="9xl">
-          {progress}%
-        </Heading>
-      </Section>
-    </motion.div>
+      <Text></Text>
+      <Heading as="span" color="white" fontSize="9xl">
+        {progress}%
+      </Heading>
+    </Section>
   );
 };
-
-// const MotionSection = motion(Section);
