e8fc06ac42db747adaafb521dd270cd838833180
Zephelion <russellnumo@outlook.com>
2024-07-04 19:46:57 +0200

    feature: about section animation wip

--- src/components/features/About/About.tsx
+++ src/components/features/About/About.tsx
@@ -1,18 +1,32 @@
-import { Text } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { Section } from "@/components/features";
+import { useInView } from "framer-motion";
+import { useRef, useEffect } from "react";
 
 interface AboutProps {
   data: {
-    content: string;
+    content: string[];
   };
 }
 
 export const About = ({ data }: AboutProps) => {
   const { content } = data;
+  const paragraphRef = useRef(null);
+  const isInView = useInView(paragraphRef, { once: true });
+
+  useEffect(() => {
+    console.log(isInView);
+  }, [isInView]);
+
   return (
     <Section spacingTop="large">
-      <Text variant="paragraph" width="40ch">
-        {content}
+      <Text ref={paragraphRef} variant="paragraph" maxWidth="40ch">
+        {content.map((line, index) => (
+          <Box as="span" key={index} overflow="hidden" bg="red">
+            <Box as="span">{line}</Box>
+            {/* {line} */}
+          </Box>
+        ))}
       </Text>
     </Section>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,13 @@ import { Hero, About } from "@/components/features";
 interface PageProps {}
 
 const aboutData = {
-  content: `Hi there! I am Russell Numo currently attending the University of applied sciences in Amsterdam. I have a bit of an affinity when it comes to frontend development especially for the web :). I am always eager to learn new things and immersive myself in new technologies. Okay now you are allowed to explore the rest of the site.`,
+  // content: `Hi there! I am Russell Numo currently attending the University of applied sciences in Amsterdam. I have a bit of an affinity when it comes to frontend development especially for the web :). I am always eager to learn new things and immersive myself in new technologies. Okay now you are allowed to explore the rest of the site.`,
+  content: [
+    "Hi there! I am Russell Numo currently attending the University of applied sciences in Amsterdam.",
+    // "I have a bit of an affinity when it comes to frontend development especially for the web :).",
+    // "I am always eager to learn new things and immersive myself in new technologies.",
+    // "Okay now you are allowed to explore the rest of the site.",
+  ],
 };
 
 const Page: NextPage<PageProps> = () => {
