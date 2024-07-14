673ba9e15cb1f894fd674cd07926eebfcdfbc688
Zephelion <russellnumo@outlook.com>
2024-07-14 21:53:42 +0200

    feature: switched to framer motion

--- src/components/features/CustomCursor/CustomCursor.tsx
+++ src/components/features/CustomCursor/CustomCursor.tsx
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
-import { useSpring, animated } from "react-spring";
+import { useSpring, motion } from "framer-motion";
 
 export const CustomCursor = () => {
   const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
 
-  // Spring animations for cursor position
-  const [{ x, y }, setSpring] = useSpring(() => ({ x: 0, y: 0 }));
+  // Framer Motion spring animation for cursor position
+  const x = useSpring(0, { stiffness: 300, damping: 30 });
+  const y = useSpring(0, { stiffness: 300, damping: 30 });
 
   useEffect(() => {
     const updateCursorPos = (e: MouseEvent) => {
       setCursorPos({ x: e.clientX, y: e.clientY });
       // Update spring values
-      setSpring({ x: e.clientX, y: e.clientY });
+      x.set(e.clientX);
+      y.set(e.clientY);
     };
 
     window.addEventListener("mousemove", updateCursorPos);
 
     return () => window.removeEventListener("mousemove", updateCursorPos);
-  }, [setSpring]);
+  }, [x, y]); // Dependencies updated to include x and y springs
 
   return (
-    <animated.div
+    <motion.div
       style={{
         position: "fixed",
         left: x,
