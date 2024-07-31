c0caa4c2a6ada6dfc44a8c978638a9c4494d5af4
Zephelion <russellnumo@outlook.com>
2024-07-31 19:13:15 +0200

    fix: dynamically set the about text based on the window size

--- src/components/features/About/About.tsx
+++ src/components/features/About/About.tsx
@@ -1,9 +1,8 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Text, useBreakpointValue } from "@chakra-ui/react";
 import { Section, StaggeredText } from "@/components/features";
 import { useInView } from "framer-motion";
 import { useRef } from "react";
 import { splitContentIntoLines } from "@/utils";
-import { useWindowSize } from "react-use";
 
 interface AboutProps {
   data: {
@@ -15,8 +14,9 @@ export const About = ({ data }: AboutProps) => {
   const { content } = data;
   const paragraphRef = useRef(null);
   const isInView = useInView(paragraphRef, { once: true, amount: 0.5 });
+  const isMobile = useBreakpointValue({ base: 25, md: 50 });
 
-  const lines = splitContentIntoLines(content, 50);
+  const lines = splitContentIntoLines(content, isMobile);
 
   return (
     <Section spacingBottom="large">
