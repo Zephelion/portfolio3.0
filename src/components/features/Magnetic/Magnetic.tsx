b60974cb6aa17ff701c761d4a02b91b318249773
Zephelion <russellnumo@outlook.com>
2024-09-04 20:06:29 +0200

    FEATURE: Magnetic component

--- src/components/features/Magnetic/Magnetic.tsx
+++ src/components/features/Magnetic/Magnetic.tsx
@@ -1,5 +1,6 @@
 import { MotionBox } from "../MotionBox";
-import { ReactNode, useState, useRef } from "react";
+import { ReactNode, useState, useRef, MouseEventHandler } from "react";
+import { motion } from "framer-motion";
 
 interface MagneticProps {
   children: ReactNode;
@@ -7,11 +8,33 @@ interface MagneticProps {
 
 export const Magnetic = ({ children }: MagneticProps) => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const boxRef = useRef(null);
+  const boxRef = useRef<HTMLDivElement>(null);
 
   const mouseMove = (e: any) => {
-    console.log("wagwan");
+    // Change any to MouseEvent
+    if (!boxRef.current) return;
+    const { clientX, clientY } = e;
+    const { width, height, left, top } = boxRef.current.getBoundingClientRect();
+    const x = clientX - (left + width / 2);
+    const y = clientY - (top + height / 2);
+    setPosition({ x, y });
   };
 
-  return <MotionBox onMouseMove={mouseMove}>{children}</MotionBox>;
+  const mouseLeave = () => {
+    setPosition({ x: 0, y: 0 });
+  };
+
+  const { x, y } = position;
+
+  return (
+    <motion.div
+      ref={boxRef}
+      onMouseMove={mouseMove}
+      onMouseLeave={mouseLeave}
+      animate={{ x, y }}
+      transition={{ type: "spring", stiffness: 200, damping: 10 }}
+    >
+      {children}
+    </motion.div>
+  );
 };
